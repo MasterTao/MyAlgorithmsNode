7c6efeab740579aaa8d72776ec92429fab190855
txu1 <txu1@i9i8.com>
2021-04-19 19:48:10 +0800

    格式化快排

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,10 +1,73 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author tao
  * @date 2021/4/13 10:13 上午
  */
 public class Solution {
+
+    public static int maximalSquare(char[][] matrix) {
+        // 枚举顶点和边长
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+
+        // 建立两个辅助数组，记录右侧，下侧连续最长的1
+        int[][] rightHelp = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            rightHelp[row][cols - 1] = matrix[row][cols - 1] == '1' ? 1 : 0;
+        }
+        for (int col = cols - 2; col >= 0; col--) {
+            for (int row = rows - 1; row >= 0; row--) {
+                rightHelp[row][col] = matrix[row][col] == '1' ? rightHelp[row][col + 1] + 1 : 0;
+            }
+        }
+
+        int maxLength = Math.min(rows, cols);
+
+        int ans = 0;
+        for (int i = 1; i <= maxLength; i++) {
+            for (int row = 0; row <= rows - i; row++) {
+                for (int col = 0; col <= cols - i; col++) {
+                    boolean flag = true;
+                    for (int tmp = row; tmp <= row + i -1; tmp++) {
+                        if (rightHelp[tmp][col] < i) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        ans = Math.max(ans, i * i);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(maximalSquare(matrix));
+
+        matrix = new char[][] {{'0', '1'}, {'1', '0'}};
+        System.out.println(maximalSquare(matrix));
+
+        matrix = new char[][] {{'0'}};
+        System.out.println(maximalSquare(matrix));
+
+        //[["1","0","1","1","0","1"],["1","1","1","1","1","1"],["0","1","1","0","1","1"],["1","1","1","0","1","0"],["0","1","1","1","1","1"],["1","1","0","1","1","1"]]
+        matrix = new char[][] {
+                {'1', '0', '1', '1', '0', '1'},
+                {'1', '1', '1', '1', '1', '1'},
+                {'0', '1', '1', '0', '1', '1'},
+                {'1', '1', '1', '0', '1', '0'},
+                {'0', '1', '1', '1', '1', '1'},
+                {'1', '1', '0', '1', '1', '1'}
+        };
+        System.out.println(maximalSquare(matrix));
+    }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,17 +1,15 @@
-package com.monkey.sort;
-
 /**
- * 快速排序
- *
  * @author tao
- * @date 2021/4/12 9:57 上午
+ * @date 2021/4/19 7:40 下午
  */
-public class Code01_QuickSort {
+public class Test {
 
     public static void quickSort(int[] arr) {
-        if (arr == null || arr.length == 0) {
+
+        if (arr == null || arr.length <= 1) {
             return;
         }
+
         process(arr, 0, arr.length - 1);
     }
 
@@ -29,16 +27,7 @@ public class Code01_QuickSort {
         process(arr, equalArea[1] + 1, R);
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-
     public static int[] partition(int[] arr, int L, int R) {
-        if (L > R) {
-            return new int[]{-1, -1};
-        }
 
         if (L == R) {
             return new int[]{L, R};
@@ -56,15 +45,24 @@ public class Code01_QuickSort {
                 index++;
             }
         }
-        swap(arr, R, moreIndex);
+        swap(arr, moreIndex, R);
         return new int[]{lessIndex + 1, moreIndex};
+
+    }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 44, 3, 2, 1};
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 44, 3, 2, 1, -1, -2, -3, -4, -5, -5, -4, -3};
         quickSort(arr);
         for (int i : arr) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 }
